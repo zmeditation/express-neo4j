216cc6185dd58a6a5cfc7f855e6c4bb747f9d3c5
zmeditation <senior.zsoftware@gmail.com>
2022-09-14 19:27:45 +0100

    create winston logger

---
+++ app/logger/index.ts
@@ -0,0 +1,26 @@
+import { createLogger, format, transports } from "winston";
+
+const ignorePrivate = format((info) => {
+  if (info.private) {
+    return false;
+  }
+  return info;
+});
+
+export const logger = createLogger({
+  transports: [
+    new transports.Console({
+      level: "info",
+      format: format.combine(
+        ignorePrivate(),
+        format.colorize(),
+        format.timestamp(),
+        format.align(),
+        format.printf((info) => {
+          const { timestamp, level, message } = info;
+          return `[${level}] ${timestamp} ${message}`;
+        })
+      ),
+    }),
+  ],
+});
